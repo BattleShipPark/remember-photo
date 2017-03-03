00b34df4273cd193bca275f8c72e091acb7a5369
BattleShip Park <battleship.park@gmail.com>
2017-03-03 14:50:40 +0900

    Add model classes

--- app/src/main/java/com/battleshippark/rememberphoto/domain/Story.java
+++ app/src/main/java/com/battleshippark/rememberphoto/domain/Story.java
@@ -2,11 +2,46 @@ package com.battleshippark.rememberphoto.domain;
 
 import com.battleshippark.rememberphoto.db.dto.StoryDto;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
 /**
  */
 
 public class Story {
-    public Story(StoryDto storyDto) {
+    private final long id;
+    private final String title;
+    private final String content;
+    private final Date date;
+    private final List<String> photoPathList;
 
+    public Story(StoryDto storyDto) {
+        id = storyDto.getId();
+        title = storyDto.getTitle();
+        content = storyDto.getContent();
+        date = storyDto.getDate();
+        photoPathList = Arrays.asList(storyDto.getPhotoPathList());
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public List<String> getPhotoPathList() {
+        return photoPathList;
+    }
+
 }
--- app/src/main/java/com/battleshippark/rememberphoto/domain/StoryList.java
+++ app/src/main/java/com/battleshippark/rememberphoto/domain/StoryList.java
@@ -1,10 +1,19 @@
 package com.battleshippark.rememberphoto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  */
 
 public class StoryList {
-    public void add(Story story) {
+    private final List<Story> storyList;
 
+    public StoryList() {
+        storyList = new ArrayList<>();
+    }
+
+    public void add(Story story) {
+        storyList.add(story);
     }
 }
